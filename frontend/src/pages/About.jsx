1a4172b352b2c77b31908793a8432205f3ba4a96
AmeyChougule07 <ameychougule14@gmail.com>
2026-01-10 21:08:07 +0530

    Completed frontend and started with backend

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Title from '../components/Title';
+import { assets } from '../assets/assets';
+import NewsletterBox from '../components/NewsletterBox';
 
 const About = () => {
   return (
-    <div></div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t '>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} className='w-full md:max-w-[450px]' alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A adipisci similique minus eligendi corrupti blanditiis quis vero molestias, amet minima?</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem id exercitationem fugiat omnis rem aliquid ducimus nihil. Laboriosam, molestiae architecto.</p>
+          <b className='text-gray-800 '>Our Mission</b>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde deleniti, ut quo non dolores eum incidunt natus, ad sequi porro officiis? Quibusdam vel ex repellendus repudiandae voluptatibus odio quos? Culpa?</p>
+        </div>
+      </div>
+      <div className='text-xl py-4 '>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border border-gray-300 px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Quality you can feel in every stitch—each piece is carefully inspected to meet our highest standards.
+            From fabric selection to final finish, we ensure lasting comfort, durability, and flawless craftsmanship.</p>
+        </div>
+        <div className='border border-gray-300 px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Convinience:</b>
+          <p className='text-gray-600'>Designed to fit seamlessly into your lifestyle, every step from selection to delivery is made simple and effortless.</p>
+        </div>
+        <div className='border border-gray-300 px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Our dedicated team is always here to listen, assist, and ensure your experience with our brand exceeds expectations.</p>
+        </div>
+      </div>
+
+      <NewsletterBox/>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
-  const { products,currency,cartItems,updateQuantity } = useContext(ShopContext);
+  const { products,currency,cartItems,updateQuantity, navigate } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
   
@@ -26,9 +27,6 @@ const Cart = () => {
     setCartData(tempData);
   }, [cartItems])
 
-{/*------------------------------I stopped here previously --------------------------*/}
-
-
   return (
     <div className='border-t pt-14'>
       <div className='text-2xl mb-3'>
@@ -37,9 +35,9 @@ const Cart = () => {
       <div className=''>
         {
           cartData.map((item,index)=>{
-            const productData = products.find((product)=> product ._id === item._id);
+            const productData = products.find((product)=> product._id === item._id);
             return (
-                <div key={index} className='py-4 "border-t border-b border-gray-400  text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div key={index} className='py-4 border-t border-b border-gray-400 text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
                   <div className='flex items-start gap-6'>
                       <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
                       <div>
@@ -50,13 +48,23 @@ const Cart = () => {
                         </div>
                       </div>
                   </div>
-                  <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 outline-none border-nono' type="number" min={1} defaultValue={item.quantity} />
+                  <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null :  updateQuantity(item._id,item.size,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 outline-none border-none' type="number" min={1} defaultValue={item.quantity} />
                   <img onClick={()=>updateQuantity(item._id,item.size,0)} src={assets.bin_icon} className='w-4 mr-4 cursor-pointer sm:w-5' alt="" />
                 </div>
             )
           })
         }
       </div>
+
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal />
+          <div className='w-full text-end'>
+            <button onClick={()=>navigate('/place-order')} className='text-white text-sm bg-black my-8 px-8 py-3 cursor-pointer'>PROCEED TO CHECKOUT</button>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
