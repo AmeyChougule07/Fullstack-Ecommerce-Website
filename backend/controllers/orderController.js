f5bb5c1367686cc320e6faf0c966627c884546ec
AmeyChougule07 <ameychougule14@gmail.com>
2026-01-23 16:19:36 +0530

    WIP: Implement demo payment gateway and order management flow

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,11 +1,16 @@
+import { response } from "express"
 import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModel.js"
+
+{/* Global variables */}
+const currency = 'inr'
+const deliveryCharge = 50
 
 {/*Placing order using cash on delivery method*/}
 
 const placeOrder = async (req,res) => {
   try {
-    const { userId,items,amount,address } = req.body
+    const { userId, items, amount, address } = req.body
     const orderData = {
       userId,
       items,
@@ -43,19 +48,40 @@ const placeOrderRazorpay = async (req,res) => {
 {/*All orders data for admin panel*/}
 
 const allOrders = async (req,res) => {
-
+  try {
+    const orders = await orderModel.find({})
+    res.json({success:true,orders})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:error.message})
+  }
 }
 
 {/*User orders data for frontend*/}
 
 const userOrders = async (req,res) => {
-
+  try {
+    const { userId } = req.body
+    const orders = await orderModel.find({ userId })
+    res.json({success:true, orders})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:error.message})
+  }
 }
 
 {/*Update order status from only admin panel*/}
 
 const updateStatus = async (req,res) => {
+  try {
+    const { orderId, status } = req.body
+    await orderModel.findByIdAndUpdate(orderId, {status})
+    res.json({success:true, message:"Status updated"})
 
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:error.message})
+  }
 }
 
 export { placeOrder,placeOrderStripe,placeOrderRazorpay,allOrders,userOrders,updateStatus }
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -3,11 +3,13 @@ import Title from '../components/Title';
 import CartTotal from '../components/CartTotal';
 import { assets } from '../assets/assets';
 import { ShopContext } from '../context/ShopContext';
+import { toast } from 'react-toastify';
+import axios from "axios"
 
 const PlaceOrder = () => {
 
   const [method, setMethod] = useState('cod');
-  const { navigate,backendUrl,token,cartItems,setCartItems,getCartamount,delivery_fee,productData } = useContext(ShopContext);
+  const { navigate,backendUrl,token,cartItems,setCartItems,getCartAmount,delivery_fee,products } = useContext(ShopContext);
   const [formData, setFormData] = useState({
     firstName:'',
     lastName:'',
@@ -32,7 +34,7 @@ const PlaceOrder = () => {
     try {
       let orderItems = []
       for(const items in cartItems){
-        for(const item in cartItems[item]){
+        for(const item in cartItems[items]){
             if (cartItems[items][item] > 0) {
               const itemInfo = structuredClone(products.find(product => product._id === items))
               if (itemInfo) {
@@ -43,11 +45,32 @@ const PlaceOrder = () => {
             }
         }
       }
-      console.log(orderItems);
+      let orderData = {
+        address: formData,
+        items: orderItems,
+        amount: getCartAmount() + delivery_fee
+      }
+
+      switch (method) {
+
+        // API calls for COD
+        case 'cod':
+          const response = await axios.post(backendUrl + '/api/order/place',orderData,{headers:{token}})
+          if (response.data.success) {
+            setCartItems({})
+            navigate('/orders')
+          } else{
+            toast.error(response.data.message)
+          }
+          break;
       
+        default:
+          break;
+      }
 
     } catch (error) {
-      
+      console.log(error);
+      toast.error(error.message);
     }
 
   }
